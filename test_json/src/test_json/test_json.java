200a02f669d75090810977b29cc2ec92eb1450d6
gaon01 <gaon01@DESKTOP-CMMT6HK>
2022-01-17 09:48:24 +0900

    send message !~!!!!

---
+++ test_json/src/test_json/test_json.java
@@ -0,0 +1,51 @@
+package test_json;
+
+import java.util.Iterator;
+import java.util.Map;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+public class test_json {
+
+	public static void main(String[] args){
+		String jsonText = "{\"acs_nastech_000\":{\"inrfId\":\"aco_nastech_u22\"},\"acs_nastech_999\":{\"sqlErrMesgCtn\":\"\",\"cct\":\"12642\",\"sqlErrNo\":\"0\"}}";
+		 
+		/*
+		 
+		JSONParser parser = new JSONParser();
+		JSONArray arr = null;
+		 
+		 
+		try {
+		     arr = (JSONArray)parser.parse(jsonText);
+		} catch (ParseException e) {
+		     System.out.println("변환에 실패");
+		     e.printStackTrace();
+		}
+		
+		System.out.println(arr);
+		System.out.println(arr.get(0));
+		System.out.println(arr.get(1));
+
+*/
+		JSONParser parser = new JSONParser();
+		String jsonString = "{\"stat\":{\"sdr\": \"aaaaaaaaaaaaaaaaaaaaa\",\"rcv\": \"bbbbbbbbbbbbbbbbbbbb\",\"time\": \"UTC in millis\"" + ",\"type\": 1,\"subt\": 1,\"argv\": [{\"1\":2},{\"2\":3}]},\"sstat\":\"1\"}"; 
+		try { 
+			Map json = (Map)parser.parse(jsonText); 
+			Iterator iter = json.entrySet().iterator(); 
+			Object entry = json.get("acs_nastech_000"); 
+			System.out.println(entry); 
+			System.out.println( entry.getClass() );
+			} catch (ParseException e) {
+				System.out.println("에러");
+				e.printStackTrace(); 
+			}
+		
+		
+		
+
+	}
+}
